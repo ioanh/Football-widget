dbebe012638bfde32a40b8b99eb7d0d1e9ee7497
Yoan Hristov <ioan.xristov@abv.bg>
2022-03-22 01:51:02 +0300

    Creating the HTML template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Application code goes here</h1>
+  <div class="container">
+    <div class="wrapper">
+      <div class="header">
+        <h1>England: Premier League</h1>
+      </div>
+      <div class="content">
+        <div class="game">
+          <p>16:00 Finished Brighton 2-1 Leicester</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,5 +32,24 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+.container{
+  display: grid;
+  background-color: red;
+  justify-content: center;
+}
 
+.wrapper{
+  align-self: center;
+  justify-self: center;
+  width: 550px;
+  height: 550px;
+  background-color: blue;
+}
+
+.header{
+  background-color: #D9D9D9;
+}
 </style>
